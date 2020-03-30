b49abd8f8546c272d6cf87667fd30fd4cd9020b2
Jesse B. Hannah <jesse@jbhannah.net>
2020-03-30 15:36:59 -0700

    Use Zerolog

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	users "github.com/koverto/users/api"
 	"github.com/koverto/users/internal/pkg/handler"
 
 	"github.com/koverto/micro"
 	"github.com/micro/go-micro/v2/config/source/env"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -18,23 +16,19 @@ func main() {
 
 	service, err := micro.NewService("com.koverto.svc.users", conf, env.NewSource(env.WithStrippedPrefix("KOVERTO")))
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not initialize service")
 	}
 
 	h, err := handler.New(conf, service)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not build handler")
 	}
 
 	if err := users.RegisterUsersHandler(service.Server(), h); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not register handler with service")
 	}
 
 	if err := service.Run(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("error running service")
 	}
 }
